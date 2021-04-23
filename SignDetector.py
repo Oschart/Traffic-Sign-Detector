c202d7c53ab8eea865793fb947b2f09f88a296bf
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2021-04-23 03:06:05 +0200

    Implemented region unification

--- SignDetector.py
+++ SignDetector.py
@@ -3,19 +3,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.io import imread, imshow
-
+from math import sqrt
 
 from SelectiveSearch import SelectiveSearch
 from SignClassifier import SignClassifier
 import cv2
 from utils import plot_showcase
 
+
 class SignDetector:
 
-    def __init__(self, pretrained = True):
+    def __init__(self, pretrained=True):
         self.sign_classifier = SignClassifier(use_cached=pretrained)
-        self.iou_th = 0.2
-        self.prob_th = 0.9
+        self.iou_th = 0.01
+        self.prob_th = 0.8
 
     def detect_signs(self, img):
         ss = SelectiveSearch()
@@ -31,40 +32,64 @@ class SignDetector:
             region_slices.append(self.extract_region(img, x, y, w, h))
 
         region_probs = self.sign_classifier.predict(region_slices)
-        rep_regions = self.unify_similar_regions(chosen_regions, region_probs)
+
+        candidates = [(region, prob, i) for i, (region, prob) in enumerate(
+            zip(regions, region_probs)) if prob >= self.prob_th]
 
         mark_img = np.array(img, copy=True)
-
-        for i, rect in enumerate(rep_regions):
+        for rect, prob, _ in candidates:
             x, y, w, h = rect
-            cv2.rectangle(mark_img, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
+            print(round((float(prob)**5)*100))
+            cv2.rectangle(mark_img, (x, y), (x+w, y+h),
+                          (255, round((float(prob)**5)*100), 0), 1, cv2.LINE_AA)
 
         plt.imshow(mark_img)
         plt.show()
 
+        rep_regions, rep_idxs = self.unify_similar_regions(candidates)
 
-    def unify_similar_regions(self, regions, region_probs):
-        rep_regions = []
-        is_grouped = [False]*len(regions)
-        for i in range(len(regions)):
-            if is_grouped[i]:
-                continue
-            mx_prob = region_probs[i]
-            rep_reg = regions[i]
-            for j in range(i+1, len(regions)):
-                if is_grouped[j]:
-                    continue
-                iou = self.iou(regions[i], regions[j])
-                if iou >= self.iou_th:
-                    if region_probs[j] > mx_prob:
-                        mx_prob = region_probs[j]
-                        rep_reg = regions[j]
-                    is_grouped[j] = True
-            if mx_prob >= self.prob_th:
-                rep_regions.append(rep_reg)
+        for i in range(len(rep_regions)):
+            for j in range(i+1, len(rep_regions)):
+                print(
+                    f'p1: {rep_regions[i]}, p2: {rep_regions[j]}, iou= {self.iou(rep_regions[i], rep_regions[j])}')
+
+        for i, rect in enumerate(rep_regions):
+            x, y, w, h = rect
+            cv2.rectangle(mark_img, (x, y), (x+w, y+h),
+                          (0, 255, 0), 1, cv2.LINE_AA)
+
+        plt.imshow(mark_img)
+        plt.show()
         return rep_regions
 
+    def unify_similar_regions(self, candidates):
+        rep_regions = []
+        rep_idxs = []
+        skip = [False]*len(candidates)
 
+        pr_func = lambda p:  sqrt(p[0][2]*p[0][3])*(p[1]**5)*(min(p[0][2],p[0][3])/max(p[0][2],p[0][3]))**6
+        candidates = sorted(
+            candidates, key=pr_func, reverse=True)
+
+        for i in range(len(candidates)):
+            if skip[i]:
+                continue
+            region1, prob1, idx1 = candidates[i]
+            area1 = region1[2]*region1[3]
+            rep_reg = region1
+            rep_idx = idx1
+            for j in range(i+1, len(candidates)):
+                if skip[j]:
+                    continue
+                region2, prob2, idx2 = candidates[j]
+                iou = self.iou(rep_reg, region2)
+
+                if iou >= self.iou_th:
+                    skip[j] = True
+            rep_regions.append(rep_reg)
+            rep_idxs.append(rep_idx)
+
+        return rep_regions, rep_idxs
 
     def extract_region(self, img, x, y, w, h):
         return img[y: y + h, x:x+w, ]
@@ -73,8 +98,13 @@ class SignDetector:
         x1, y1, w1, h1 = rect1
         x2, y2, w2, h2 = rect2
 
-        interx = max(0, (w1 if x1 <= x2 else w2) - abs(x1-x2))
-        intery = max(0, (h1 if y1 <= y2 else h2) - abs(y1-y2))
+        if x1 <= x2 and y1 <= y2:
+            return 1.0
+        elif x1 >= x2 and y1 >= y2:
+            return 1.0
+        else:
+            interx = max(0, (w1 if x1 <= x2 else w2) - abs(x1-x2))
+            intery = max(0, (h1 if y1 <= y2 else h2) - abs(y1-y2))
 
         inter_area = interx*intery
         area1 = w1*h1
