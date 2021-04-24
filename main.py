02b62c073ca9d72476b488c6c95a703bf3a2911c
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2021-04-24 04:20:58 +0200

    Automated test runs

--- main.py
+++ main.py
@@ -9,19 +9,28 @@ from SelectiveSearch import SelectiveSearch
 from SignClassifier import SignClassifier
 from SignDetector import SignDetector
 import cv2
-from utils import plot_showcase
+from tqdm import tqdm
+import os
+
+def run(tests_path='test_cases'):
+    sd = SignDetector(pretrained=True)
+
+    tests = [test.name for test in os.scandir(
+                tests_path) if test.is_file()]
+    for i, img_fname in tqdm(enumerate(tests)):
+        tdir = '/'.join([tests_path, img_fname])
+        save_dir = '/'.join(['test_results', img_fname])
+        save_dirc = '/'.join(['test_results_canny', img_fname])
+        save_dirt = '/'.join(['test_results_t', img_fname])
+        print(f'Test #{i+1} ===>> {img_fname}')
+        img = cv2.imread(tdir)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        bboxes, annotated_img, canny_img, imgt = sd.detect_signs(img)
+        plt.imsave(save_dir, annotated_img)
+        plt.imsave(save_dirc, canny_img)
+        plt.imsave(save_dirt, imgt)
+        print('Bounding boxes (x,y,w,h):')
+        print(bboxes)
 
 
-
-
-img_path = 'test_cases/Am_Rojo0024.jpg'
-orig_img = cv2.imread(img_path)
-
-
-newHeight = 800
-newWidth = int(orig_img.shape[1]*newHeight/orig_img.shape[0])
-orig_img = cv2.resize(orig_img, (newWidth, newHeight))
-orig_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB)
-
-sd = SignDetector(pretrained=True)
-print(sd.detect_signs(orig_img))
+run()
